7afd24448d90d82a3bf1709ba906ac204f0fc3d6
RecursiveTimeGhost <hh0ecx45@duck.com>
2023-11-25 12:51:41 -0500

    Updated markup, CSS; added blogs;

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 const TITLE       = 'Recursive Time Ghost';
 const DESCRIPTION = 'A tiny blog for nerds.';
 
+// Function to set the target attribute of all links to "_blank"
+   const setLinksTargetBlank = () =>
+      {
+      // Assuming your footer has an ID, replace "yourFooterId" with the actual ID
+      var footer = document.getElementsByTagName("footer");
 
- // Function to set the target attribute of all links to "_blank"
-    const setLinksTargetBlank = () =>
-        {
-        document.querySelectorAll('a').forEach(link => {link.target = '_blank';});
-        }
+      Array.from(footer).forEach((f) => 
+         {
+      // Select all anchor elements within the footer
+         var links = f.getElementsByTagName('a');
+      
+      // If you want to do something specific with each link, you can iterate through them
+         Array.from(links).forEach((l) => {console.log(l.href);});
+         });
+      }
 
- // Call the function when the page has fully loaded
-    window.addEventListener('load', setLinksTargetBlank);
+// Function to set the target attribute of all links to "_blank"
+const handleWindowLoadEvent = () =>
+{
+try
+   {
+   setLinksTargetBlank ();
+   }
+catch (error)
+   {
+   console.error(error.message);
+   }
+}
+
+// Call the function when the page has fully loaded
+    window.addEventListener('load', handleWindowLoadEvent);
 
